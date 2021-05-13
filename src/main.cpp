f5ed2e708bbca812bf3328719a9dc5111c2e762c
topeixoto <taiane.o.p@gmail.com>
2021-05-13 20:21:09 -0300

    add basic structres

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,16 @@
 // Headers locais, definidos na pasta "include/"
 #include "utils.h"
 #include "matrices.h"
+struct SceneObject
+{
+    std::string name;              // Nome do objeto
+    size_t first_index;            // Índice do primeiro vértice dentro do vetor indices[] definido em BuildTrianglesAndAddToVirtualScene()
+    size_t num_indices;            // Número de índices do objeto dentro do vetor indices[] definido em BuildTrianglesAndAddToVirtualScene()
+    GLenum rendering_mode;         // Modo de rasterização (GL_TRIANGLES, GL_TRIANGLE_STRIP, etc.)
+    GLuint vertex_array_object_id; // ID do VAO onde estão armazenados os atributos do modelo
+    glm::vec3 bbox_min;            // Axis-Aligned Bounding Box do objeto
+    glm::vec3 bbox_max;
+};
 
 // Estrutura que representa um modelo geométrico carregado a partir de um
 // arquivo ".obj". Veja https://en.wikipedia.org/wiki/Wavefront_.obj_file .
@@ -51,7 +61,6 @@ struct ObjModel
     tinyobj::attrib_t attrib;
     std::vector<tinyobj::shape_t> shapes;
     std::vector<tinyobj::material_t> materials;
-
     // Este construtor lê o modelo de um arquivo utilizando a biblioteca tinyobjloader.
     // Veja: https://github.com/syoyo/tinyobjloader
     ObjModel(const char *filename, const char *basepath = NULL, bool triangulate = true)
@@ -69,6 +78,27 @@ struct ObjModel
 
         printf("OK.\n");
     }
+};
+
+struct Position
+{
+    int x;
+    int y;
+    int z;
+};
+
+struct Player
+{
+    int score = 0;
+    int lives = 3;
+    Position position;
+    ObjModel *model;
+};
+
+struct Character
+{
+    Position position;
+    ObjModel *model;
 };
 
 // Declaração de funções utilizadas para pilha de matrizes de modelagem.
@@ -115,19 +145,6 @@ void MouseButtonCallback(GLFWwindow *window, int button, int action, int mods);
 void CursorPosCallback(GLFWwindow *window, double xpos, double ypos);
 void ScrollCallback(GLFWwindow *window, double xoffset, double yoffset);
 void startGame();
-
-// Definimos uma estrutura que armazenará dados necessários para renderizar
-// cada objeto da cena virtual.
-struct SceneObject
-{
-    std::string name;              // Nome do objeto
-    size_t first_index;            // Índice do primeiro vértice dentro do vetor indices[] definido em BuildTrianglesAndAddToVirtualScene()
-    size_t num_indices;            // Número de índices do objeto dentro do vetor indices[] definido em BuildTrianglesAndAddToVirtualScene()
-    GLenum rendering_mode;         // Modo de rasterização (GL_TRIANGLES, GL_TRIANGLE_STRIP, etc.)
-    GLuint vertex_array_object_id; // ID do VAO onde estão armazenados os atributos do modelo
-    glm::vec3 bbox_min;            // Axis-Aligned Bounding Box do objeto
-    glm::vec3 bbox_max;
-};
 
 // Abaixo definimos variáveis globais utilizadas em várias funções do código.
 
